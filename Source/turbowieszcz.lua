aea40204742da48ba44c130cf39875057d8963e5
Jakub Noniewicz <kuba@kid.com.pl>
2022-05-13 11:21:02 +0200

    misspelling fix

--- Source/turbowieszcz.lua
+++ Source/turbowieszcz.lua
@@ -1,4 +1,4 @@
-
+﻿
 -- Turbo Wieszcz ++ LUA version, v1.0
 -- (c)2022 Noniewicz.com, Jakub Noniewicz
 
@@ -15,7 +15,7 @@
 -- https://www.tutorialspoint.com/execute_lua_online.php
 
 -- created: 20220501 (initial release took: 2.5h)
--- updated: 20220502 (prep for Playdate console)
+-- updated: 20220502, 13 (prep for Playdate console)
 
 
 ENDINGS1 = {'.', '...', '.', '!', '.'}
@@ -99,7 +99,7 @@ end
     data[0][28] = 'Gdy nie każdego stać na browary'
     data[0][29] = 'Pożarł Hilary swe okulary'
     data[0][30] = 'Spowiły nas trujące opary'
-    data[0][31] = 'To nie jest calka ani logarytm'
+    data[0][31] = 'To nie jest całka ani logarytm'
 -- ///////////////////////////////////////////////
 -- //po 8
     data[1][0]  = 'Już na arenę krew tryska'
